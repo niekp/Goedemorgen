ca9c8c8156b8baae93bb932d97c723b8206604fb
Niek <niek@000101010.com>
2018-08-23 20:29:08 +0200

    Bash en php script voor downtime toegevoegd als comment

--- modules/Downtime.py
+++ modules/Downtime.py
@@ -29,3 +29,48 @@ class Downtime(Module):
 
 	def GetText(self):
 		return super(Downtime, self).GetText()
+
+
+"""
+##### De server
+ping.sh - Op de server/raspberry pi in de cronjob zetten:
+
+# apparaat.txt in de homedirectory maken met hierin de naam van het apparaat
+
+# IP opslaan in een txt file
+/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' > /home/niek/ip.txt
+
+# Uptime opslaan in een txt
+echo "Uptime: " >/home/niek/extra.txt
+/usr/bin/uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print $2" "$3 }' >> /home/niek/extra.txt
+
+# Beschikbare schijfruimte naar txt
+echo "Free diskspace: " >> /home/niek/extra.txt
+/bin/df -h | tr -s ' ' $'\t' | grep /dev/root | cut -f4 >> /home/niek/extra.txt
+
+# Ping versturen naar de server
+curl -d "apparaat=$(cat /home/niek/apparaat.txt)&extra=$(cat /home/niek/extra.txt)&ip=$(cat /home/niek/ip.txt)" -X POST https://.... php file
+
+-------------------------------------------------------
+##### De PHP file
+<?php
+header('Content-Type: application/json');
+if (isset($_REQUEST["apparaat"])) {
+	
+	$ping = json_decode(file_get_contents('ping.json'), true);
+	$apparaat = strtolower($_REQUEST["apparaat"]);
+	$ping[$apparaat]["ip"] = $_REQUEST["ip"];
+
+	if (isset($_REQUEST["extra"]))
+		$ping[$apparaat]["extra"] = $_REQUEST["extra"];
+	
+	$ping[$apparaat]["ping"] = time();
+	
+	if(file_put_contents('ping.json', json_encode($ping, JSON_PRETTY_PRINT))) {
+		echo '{ "ret": "pong" }';
+	} else {
+		echo '{ "ret": "error" }';
+	}
+}
+?>
+"""

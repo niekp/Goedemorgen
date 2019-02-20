929e9f6c2992b5fbc5faed8234263dea74d585e3
Niek <niek@000101010.com>
2019-02-20 20:42:18 +0100

    comment verwijderen

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,14 +5,3 @@ from modules._Module import _Module
 
 files = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in files if isfile(f) and not f.endswith('__init__.py') and not f.endswith('_Module.py')]
-
-#from modules.Afval import Afval
-#from modules.Downtime import Downtime
-#from modules.Weer import Weer
-#from modules.Agenda import Agenda
-#from modules.LastFmDisconnected import LastFmDisconnected
-#from modules.MarkdownTodo import MarkdownTodo
-#from modules.Muspy import Muspy
-#from modules.SociaalWerker import SociaalWerker
-#from modules.LastFmRecommendation import LastFmRecommendation
-#from modules.Syncthing import Syncthing
